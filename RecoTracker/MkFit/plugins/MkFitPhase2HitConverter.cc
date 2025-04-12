ee2132b481f7075c15bb45d6e7815ba4d524b30d
Enrico Lusiani <enrico.lusiani@cern.ch>
2025-04-13 00:24:07 +0200

    Preallocate containers in MkFitEventOfHitsProducer

--- RecoTracker/MkFit/plugins/MkFitPhase2HitConverter.cc
+++ RecoTracker/MkFit/plugins/MkFitPhase2HitConverter.cc
@@ -52,6 +52,7 @@ private:
   const edm::EDPutTokenT<MkFitHitWrapper> wrapperPutToken_;
   const edm::EDPutTokenT<MkFitClusterIndexToHit> clusterIndexPutToken_;
   const edm::EDPutTokenT<std::vector<int>> layerIndexPutToken_;
+  const edm::EDPutTokenT<std::vector<unsigned int>> layerSizePutToken_;
   const edm::EDPutTokenT<std::vector<float>> clusterChargePutToken_;
   const ConvertHitTraitsPhase2 convertTraits_;
 };
@@ -65,6 +66,7 @@ MkFitPhase2HitConverter::MkFitPhase2HitConverter(edm::ParameterSet const& iConfi
       wrapperPutToken_{produces()},
       clusterIndexPutToken_{produces()},
       layerIndexPutToken_{produces()},
+      layerSizePutToken_{produces()},
       clusterChargePutToken_{produces()},
       convertTraits_{} {}
 
@@ -87,6 +89,7 @@ void MkFitPhase2HitConverter::produce(edm::StreamID iID, edm::Event& iEvent, con
   MkFitClusterIndexToHit clusterIndexToHit;
   std::vector<int> layerIndexToHit;
   std::vector<float> clusterCharge;
+  std::vector<unsigned int> layerSize(mkFitGeom.trackerInfo().n_layers(), 0);
 
   edm::ProductID stripClusterID;
   const auto& phase2Hits = iEvent.get(siPhase2RecHitToken_);
@@ -98,6 +101,7 @@ void MkFitPhase2HitConverter::produce(edm::StreamID iID, edm::Event& iEvent, con
                                         hitWrapper.hits(),
                                         clusterIndexToHit.hits(),
                                         layerIndexToHit,
+                                        layerSize,
                                         dummy,
                                         ttopo,
                                         ttrhBuilder,
@@ -111,6 +115,7 @@ void MkFitPhase2HitConverter::produce(edm::StreamID iID, edm::Event& iEvent, con
   iEvent.emplace(clusterIndexPutToken_, std::move(clusterIndexToHit));
   iEvent.emplace(layerIndexPutToken_, std::move(layerIndexToHit));
   iEvent.emplace(clusterChargePutToken_, std::move(clusterCharge));
+  iEvent.emplace(layerSizePutToken_, std::move(layerSize));
 }
 
 DEFINE_FWK_MODULE(MkFitPhase2HitConverter);
